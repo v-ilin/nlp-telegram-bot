0b899c90c968afe3ba469b01db68aa83298a8f2f
v-ilin <xrigikx@mail.ru>
2018-12-25 22:00:36 +0100

    Added config template.

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ import logging
 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 
-TOKEN = ''
-BASE_URL = ''
+from config import config
 
-updater = Updater(TOKEN, BASE_URL)
+
+updater = Updater(config.TELEGRAM_BOT_TOKEN)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                    level=logging.INFO)
+
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+
 start_handler = CommandHandler('start', start)
+echo_handler = MessageHandler(Filters.text, echo)
+
+dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(start_handler)
 
 updater.start_polling()
